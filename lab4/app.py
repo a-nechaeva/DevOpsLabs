0cc538f6ae8d8b6cb542bdc5b61f390ddc1944dd
anechaeva <312298@niuitmo.ru>
2026-05-27 02:08:40 +0300

    upd python files

--- lab4/app.py
+++ lab4/app.py
@@ -13,5 +13,6 @@ def meo():
 def health():
     return "OK", 200
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- lab4/test_app.py
+++ lab4/test_app.py
@@ -13,7 +13,7 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'^..^' in response.data
-    
+
 
 def test_health(client):
     response = client.get('/health')
